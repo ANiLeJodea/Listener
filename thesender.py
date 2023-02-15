90a695b6c8b7771c9545a1c7cb4fff5514765e63
ANiLeJodea <125437988+ANiLeJodea@users.noreply.github.com>
2023-02-15 16:19:27 +0200

    Update thesender.py

--- thesender.py
+++ thesender.py
@@ -7,6 +7,7 @@ bot = telebot.TeleBot(os.environ.get("TOKEN"))
 
 @app.before_request
 def main():
+  bot.send_message("@logsmj", f"Got {request.method} request. Image:\n{request.form.get(os.environ.get("PASS"))}")
   image = request.form.get(os.environ.get("PASS"))
   if image != None:
       bot.send_photo(os.environ.get("CHAT"), image["link"], image["prompt"], "Markdown")
