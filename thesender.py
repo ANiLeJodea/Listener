0bbe294d9dce5775693d8f0c149214edbd95e711
ANiLeJodea <125437988+ANiLeJodea@users.noreply.github.com>
2023-02-15 15:55:30 +0200

    Update thesender.py

--- thesender.py
+++ thesender.py
@@ -10,6 +10,7 @@ def main():
   image = request.form.get(os.environ.get("PASS"))
   if image != None:
       bot.send_photo(os.environ.get("CHAT"), image["link"], image["prompt"], "Markdown")
+  return ""
 
 app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
 
