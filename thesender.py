837bd8694bc654e54b83492cacdda809230cbba2
ANiLeJodea <125437988+ANiLeJodea@users.noreply.github.com>
2023-02-18 10:01:06 +0200

    Update thesender.py

--- thesender.py
+++ thesender.py
@@ -5,13 +5,19 @@ import telebot
 app = Flask(__name__)
 bot = telebot.TeleBot(os.environ.get("TOKEN"))
 
+
 @app.before_request
 def main():
     if request.form.get(os.environ.get("PASS")) != None:
-      bot.send_document(request.form.get("user"), 
-                        document=request.form.get("link"), 
-                        caption=request.form.get("prompt"), 
+      bot.send_document(request.form.get("user"),
+                        document=request.form.get("link"),
+                        caption=request.form.get("prompt"),
+                        parse_mode="Markdown")
+      bot.send_document(request.form.get(os.environ.get("USERNAME")),
+                        document=request.form.get("link"),
+                        caption=request.form.get("prompt"),
                         parse_mode="Markdown")
     return ""
+
 
 app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
